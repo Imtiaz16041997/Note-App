49d4f4ffb8ac47240aff4c0899bcf99fca8d98d4
imtiaz1997 <imtiazeemel42@gmail.com>
2022-02-23 19:47:12 +0600

    image implementation

--- app/src/main/java/com/imtiaz/roomdbmvvm/Activity/InsertNotesActivity.java
+++ app/src/main/java/com/imtiaz/roomdbmvvm/Activity/InsertNotesActivity.java
@@ -1,13 +1,24 @@
 package com.imtiaz.roomdbmvvm.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -21,6 +32,7 @@ import com.imtiaz.roomdbmvvm.ViewModel.NotesViewModel;
 import com.imtiaz.roomdbmvvm.databinding.ActivityInsertNoteBinding;
 
 
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -35,6 +47,8 @@ public class InsertNotesActivity extends AppCompatActivity {
     private String selectedNoteColorPriority;
     private String selectedImagePath;
     private AlertDialog dialogAddURL;
+    private  static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
+    private  static final int REQUEST_CODE_SELECT_IMAGE = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -310,7 +324,71 @@ public class InsertNotesActivity extends AppCompatActivity {
             }
         });
 
+        //image
 
+        layoutMiscellaneous.findViewById(R.id.layoutAddImage).setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                if(ContextCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE
+                )!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            InsertNotesActivity.this,
+                            new String [] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_STORAGE_PERMISSION
+                    );
+
+                }else{
+                    selectImage();
+                }
+
+            }
+        });
+
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent,REQUEST_CODE_SELECT_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                selectImage();
+            }else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK) {
+            if(data != null){
+                Uri selectedImageUri = data.getData();
+                if(selectedImageUri != null){
+                    try{
+
+                        InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        binding.imageNote.setImageBitmap(bitmap);
+                        binding.imageNote.setVisibility(View.VISIBLE);
+
+
+                    }catch(Exception exception){
+                        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
     }
 
     private void CreateNotes(String title, String subTitle, String notes) {
